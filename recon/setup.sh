233e7f1d8953012ed428af117877b164f6cba8c2
tiredperson47 <155717738+tiredperson47@users.noreply.github.com>
2024-12-27 01:15:02 -0800

    Update setup.sh
    unrelated note, for the iphunter.exe, the banner prints at the very end. Idk why

--- recon/setup.sh
+++ recon/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc -o iphunter.so lin_iphunter.c -lp -pthread
+gcc -o iphunter.so linux_iphunter.c -lp -pthread
 x86_64-w64-mingw32-gcc -o iphunter.exe win_iphunter.c -static -lm -pthread -lws2_32
